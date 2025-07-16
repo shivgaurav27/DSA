d96a9a788eae070b9ad041bf78f0814a93f3a9bb
shiv7727 <shiv7727@gmail.com>
2025-07-16 11:25:54 +0530

    added

--- DSA/Arrays/Update_Replace_a_Specific_Element_in_an_Array/using-array-index.js
+++ DSA/Arrays/Update_Replace_a_Specific_Element_in_an_Array/using-array-index.js
@@ -5,3 +5,19 @@ const games = ['Elden Ring', 'GTA 5', 'Horizon Forbidden West'];
 games[1] = 'Pokemon Go';
 
 console.log(games);
+
+/***
+ *  if you have to find index you can use
+ * indexOf to find the index of item
+ */
+
+// e.g
+
+// const indexOFGTA5 = games.indexOf('GTA 5');
+// console.log('indexOFGTA5', indexOFGTA5);
+
+// if (indexOFGTA5 > -1) {
+// 	games[indexOFGTA5] = 'Duck Duck Go';
+// }
+
+// console.log(games);
---
+++ DSA/Arrays/Update_Replace_a_Specific_Element_in_an_Array/using-splice-method.js
@@ -0,0 +1,5 @@
+let myArray = [10, 20, 30, 40, 50];
+// Replace the 4th element (whose index = 3) with 2500
+
+myArray.splice(3, 1, 2500);
+console.log(myArray);
